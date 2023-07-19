6ba5c5f5a5be6814540e1830fae10d006240c45e
YuliiaDubyniuk <126833026+YuliiaDubyniuk@users.noreply.github.com>
2023-07-19 11:51:12 -0400

    everything works. hw done

--- js/task-10.js
+++ js/task-10.js
@@ -1,31 +1,35 @@
 const inputEl = document.querySelector("input");
 const btnCreateEl = document.querySelector("[data-create]");
 const btnDestroyEl = document.querySelector("[data-destroy]");
-const divBoxEl = document.querySelector('#controls');
+const divBoxEl = document.querySelector('#boxes');
 
 btnCreateEl.addEventListener('click', createBoxes);
 btnDestroyEl.addEventListener('click', destroyBoxes);
-
-const color = getRandomHexColor();
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
+};
 
-function createBoxes(amount) {
-  amount = inputEl.value;
+function createBoxes() {
+  const amount = inputEl.value;
+  const boxesArray = [];
+  
   for (let i = 1; i <= amount; i += 1) {
+    const size = 30 + i * 10;
     const newDiv = document.createElement("div");
-    newDiv.style.backgroundColor = color;
-    newDiv.style.width = calc(30 + i * 10) + "px";
-    newDiv.style.height = calc(30 + i * 10) + "px";
-    divBoxEl.append(newDiv);
-  }
+    newDiv.style.backgroundColor = getRandomHexColor();
+    newDiv.style.width = `${size}px`;
+    newDiv.style.height = `${size}px`;
+    boxesArray.push(newDiv);
+  };
+
+  divBoxEl.append(...boxesArray);
 };
 
 function destroyBoxes() {
   divBoxEl.innerHTML = "";
+  inputEl.value = "";
 };
 
